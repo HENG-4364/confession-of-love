4e5bf4238b572f7bf79425befbae4a9aeb349b61
Lyheng <senglyheng2001@gmail.com>
2024-02-13 13:44:51 +0700

    first-commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import HomeScreen from "@/screen/HomeScreen";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <HomeScreen />
+    </>
+  );
+}
---
+++ src/screen/HomeScreen.tsx
@@ -0,0 +1,53 @@
+"use client";
+import { useEffect, useState } from "react"
+import "./home_screen.scss"
+
+const HomeScreen = () => {
+  const [title, setTitle] = useState(true)
+  const [gif, setGif] = useState("https://media4.giphy.com/media/CJ5bKVKLSQsrs3nJw2/giphy.gif?cid=ecf05e47rly5v8gd0bphd5ct7n85siobd7eyy0xe3hkml64d&ep=v1_gifs_related&rid=giphy.gif&ct=s")
+  const handleYesClick = () => {
+    setTitle(!true);
+    setGif(
+      'https://media3.giphy.com/media/jyuuoHidVXPuJ2iBRT/giphy.gif?cid=ecf05e470ggvsfgcfvaku3680ddxs6r8epgs7adgdpeapi89&ep=v1_gifs_related&rid=giphy.gif&ct=s'
+    );
+  }
+  const handleNoMouseOver = () => {
+
+    const noBtn = document.querySelector('.no-btn') as HTMLButtonElement;
+
+    if (noBtn) {
+      const noBtnRect = noBtn.getBoundingClientRect();
+      const maxX = window.innerWidth - noBtnRect.width;
+      const maxY = window.innerHeight - noBtnRect.height;
+      const randomX = Math.floor(Math.random() * maxX);
+      const randomY = Math.floor(Math.random() * maxY);
+      noBtn.style.left = randomX + 'px';
+      noBtn.style.top = randomY + 'px';
+    }
+
+  };
+  return (
+    <div className="wrapper">
+      <h2 className="question">{
+        title ? `${"តើអ្នកព្រមធ្វើសង្សារខ្ញុំទេ?"}` : `${"ខ្ញុំក៏ស្រលាញ់អ្នកដែរ! ❤️"}`
+      }</h2>
+      <img
+        className="gif"
+        alt="gif"
+        src={gif} />
+      <div className={`${title? "":"hidden"}`}>
+        <div className="btn-group">
+          <button className="yes-btn" onClick={handleYesClick} >
+            យល់ព្រម
+          </button>
+          <button className="no-btn" onMouseOver={handleNoMouseOver} >
+            ទេ
+          </button>
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default HomeScreen
